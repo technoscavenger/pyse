f259fad6b49498c72d2da53415a75af8d84094b5
technoscavenger <techno.scavenger@gmail.com>
2019-12-19 00:16:12 -0600

    Fix formatting

--- html5geek.py
+++ html5geek.py
@@ -2,7 +2,7 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 
 
-class TestTest():
+class TestTest:
     def setup_method(self, method):
         self.driver = webdriver.Chrome()
         self.vars = {}
